d1b9ddb3bf5cb9a14dfe1d8b4f72848f7fbf68ee
DZittersteyn <Zittersteyn@gmail.com>
2012-07-08 13:48:20 +0200

    1-5-15 min content, with shiny images

--- html-resume/resume.js
+++ html-resume/resume.js
@@ -2,25 +2,21 @@ $(function(){
 
 
 	$('#time').change(function(obj){
+		$('.1, .5, .15').hide();
 		if(obj.target.value == 1){
 			$("#minutes").html("");
 			$(".1" ).show();
-			$(".5" ).hide();
-			$(".15").hide();
 		}else if(obj.target.value == 5){
 			$("#minutes").html("s");
-			$(".1" ).show();
 			$(".5" ).show();
-			$(".15").hide();
 		}else if(obj.target.value == 15){
 			$("#minutes").html("s");
-			$(".1" ).show();
-			$(".5" ).show();
-			$(".15").show();
+			$(".15" ).show();		
 		}
 	});
 
 
 	$('#time').prop('selectedIndex','1');
+	$('#time').change();
 
 });
