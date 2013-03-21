f70d9274829a039c41220b0d5e77614ddc1d9a57
Dirk Zittersteyn <zittersteyn@gmail.com>
2013-03-21 18:29:33 +0100

    Updated some experience, graceful degradation on non-JS

--- html-resume/resume.js
+++ html-resume/resume.js
@@ -1,5 +1,11 @@
 $(function(){
-
+	$('#timeselect').html(	'I have ' +
+							'<select id="time"> ' +
+								'<option value="1" >  1 </option>' +
+								'<option value="5" >  5 </option>' +
+								'<option value="15"> 15 </option>' +
+							'</select>' +
+							' minute<span id="minutes">s</span> to read this');
 
 	$('#time').change(function(obj){
 		$('.1, .5, .15').hide();
@@ -11,7 +17,7 @@ $(function(){
 			$(".5" ).show();
 		}else if(obj.target.value == 15){
 			$("#minutes").html("s");
-			$(".15" ).show();		
+			$(".15" ).show();
 		}
 	});
 
